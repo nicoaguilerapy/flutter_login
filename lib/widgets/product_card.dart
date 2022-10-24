0f7fd541bf3bca2aaf584c7710381e37b2b9816a
uverodev <60054614+nicoaguilerapy@users.noreply.github.com>
2022-10-23 22:20:12 -0300

    video 2, parte 1
    Se realizo el card de producto

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -12,8 +12,20 @@ class ProductCard extends StatelessWidget {
         decoration: _cardBorders(),
         child: Stack(
           alignment: Alignment.bottomLeft,
-          children: [_BackgroundImage(), _ProductDetails()],
-
+          children: [
+            _BackgroundImage(),
+            _ProductDetails(),
+            Positioned(
+              child: _PriceTag(),
+              top: 0,
+              right: 0,
+            ),
+            Positioned(
+              child: _NotAvailable(),
+              top: 0,
+              left: 0,
+            ),
+          ],
         ),
       ),
     );
@@ -27,30 +39,105 @@ class ProductCard extends StatelessWidget {
           ]);
 }
 
-class _BackgroundImage extends StatelessWidget {
+class _NotAvailable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
+      width: 100,
       height: 70,
-      color: Colors.indigo,
-      );
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.redAccent,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25),
+            bottomRight: Radius.circular(25),
+          )),
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'NO DISPONIBLE',
+            style: TextStyle(color: Colors.white, fontSize: 10),
+          ),
+        ),
+      ),
+    );
   }
+}
 
+class _PriceTag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(25),
+            bottomLeft: Radius.circular(25),
+          )),
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'USD 103,5',
+            style: TextStyle(color: Colors.white, fontSize: 10),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class _ProductDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(25),
+    return Padding(
+      padding: EdgeInsets.only(right: 50),
       child: Container(
-        width: double.infinity,
-      height: 400,
-      child: FadeInImage(
-          placeholder: AssetImage('assets/jar-loading.gif'),
-          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6')),)
-      
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          width: double.infinity,
+          height: 70,
+          decoration: _buildBoxDecoration(),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text('TITULO',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis),
+            Text(
+              'id del duro duro',
+              style: TextStyle(fontSize: 10),
+            ),
+          ])),
     );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+      color: Colors.indigo,
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25), topRight: Radius.circular(25)));
+}
+
+class _BackgroundImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(25),
+        child: Container(
+          width: double.infinity,
+          height: 400,
+          child: FadeInImage(
+              placeholder: AssetImage('???'),
+              image: NetworkImage('http://via.placeholder.com/400x300/f6f6f6')),
+        ));
   }
 }
